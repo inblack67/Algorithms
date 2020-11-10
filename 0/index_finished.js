3dfb701724b393892e4d16a7a22965374f5da907
Aman Bhardwaj <devproddeploy@gmail.com>
2020-11-10 19:51:19 +0530

    did you miss me

--- 0/index_finished.js
+++ 0/index_finished.js
@@ -1,67 +1,6 @@
 // CHALLENGE 1: ADD ALL NUMBERS
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
-
-// Solution 1: ES5 arguments & for loop
-function addAll() {
-  var args = Array.prototype.slice.call(arguments);
-  var total = 0;
-  for (let i = 0; i < args.length; i++) {
-    total += args[i];
-  }
-  return total;
-}
-
-// Solution 2: ...rest & reduce/forEach
-function addAll(...numbers) {
-  return numbers.reduce((acc, cur) => acc + cur);
-}
-
-// CHALLENGE 2: SUM ALL PRIMES
-// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
-// ex. sumAllPrimes(10) == 17
-
-function sumAllPrimes(num) {
-  let total = 0;
-
-  function checkForPrime(i) {
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  for (let i = 2; i <= num; i++) {
-    if (checkForPrime(i)) {
-      total += i;
-    }
-  }
-  return total;
-}
-
-// CHALLENGE 3: SEEK & DESTROY
-// Remove from the array whatever is in the following arguments. Return the leftover values in an array
-// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
-
-// Solution 1: arguments, indexOf, filter
-function seekAndDestroy(arr) {
-  const args = Array.from(arguments);
-
-  function filterArr(arr) {
-    // Return true if NOT in array
-    return args.indexOf(arr) === -1;
-  }
-
-  return arr.filter(filterArr);
-}
-
-// Solution 2: ...rest, filter & includes
-function seekAndDestroy(arr, ...rest) {
-  return arr.filter(val => !rest.includes(val));
-}
-
 // CHALLENGE 4: SORT BY HEIGHT
 // Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
 // ex.
@@ -101,18 +40,4 @@ function missingLetters(str) {
   });
 
   return missing;
-}
-
-// CHALLENGE 6: EVEN & ODD SUMS
-// Take in an array and return an array of the sums of even and odd numbers
-// ex.
-// evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
-
-function evenOddSums(arr) {
-  let evenSum = 0;
-  let oddSum = 0;
-
-  arr.forEach(num => (num % 2 === 0 ? (evenSum += num) : (oddSum += num)));
-
-  return [evenSum, oddSum];
 }
---
+++ MissingLetters/readme.md
@@ -0,0 +1,6 @@
+## MISSING LETTERS
+- Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
+ex.
+missingLetters("abce") == "d"
+missingLetters("abcdefghjklmno") == "i"
+missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
